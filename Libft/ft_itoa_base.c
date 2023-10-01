9e41cd73dd334158851a5aadcf1a4ed64bb28018
Borja Velasco <12027006-bvelasco@noreply.gitlab.com>
2023-10-01 19:17:43 +0200

    removed unnecesary function ft_str_prot and now ft_itoa_base accept all bases and not only hexadecimal

--- Libft/ft_itoa_base.c
+++ Libft/ft_itoa_base.c
@@ -6,43 +6,34 @@
 /*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 11:13:41 by adiaz-lo          #+#    #+#             */
-/*   Updated: 2023/09/14 11:13:42 by adiaz-lo         ###   ########.fr       */
+/*   Updated: 2023/10/01 18:56:40 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	*ft_str_prot(char *str)
-{
-	if (!str)
-	{
-		free(str);
-		return (NULL);
-	}
-}
 
 char		*ft_itoa_base(unsigned long nb, char *base)
 {
 	char			*hex;
 	unsigned long	nbr;
 	unsigned long	len;
+	const size_t	base_len = ft_strlen(base);
 
 	len = 0;
 	nbr = nb;
 	while (nb)
 	{
 		len++;
-		nb /= 16;
+		nb /= base_len;
 	}
 	if (nbr == 0)
 		len++;
 	hex = malloc(sizeof(char) * (len + 1));
-	ft_str_prot(hex);
 	hex[len] = '\0';
 	while (nbr != 0 || len)
 	{
 		hex[--len] = base[nbr % 16];
-		nbr /= 16;
+		nbr /= base_len;
 	}
 	return (hex);
 }
